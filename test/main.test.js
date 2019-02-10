242708c7c4f61c9c7412f4fc8932b68d96bba5bd
stonefarmer9 <g.p.levaillant@gmail.com>
2019-02-10 11:11:31 +0000

    First feature test - Gutter game returns 0 - failing

--- test/main.test.js
+++ test/main.test.js
@@ -1,5 +1,14 @@
-const foo = require('../scripts/main.js');
+const foo = require('../scripts/main.js')
 
-test('it returns 90', () => {
-  expect(foo()).toBe(90);
+describe('It calculates scores from 20 rolls', () => {
+  test('it returns 0 for a gutter game', () => {
+    var game = new Game()
+    var roll = 0
+    do {
+      game.roll(0)
+      roll += 1
+    } while (roll <= 20)
+
+    expect(game.score()).toBe(0)
+  })
 })
