eccae90da2f3c23bfab5d6935aa446d188f91c91
stonefarmer9 <g.p.levaillant@gmail.com>
2019-02-10 12:08:30 +0000

    unit test for #score - it returns total of scorecard - failing

--- test/units/game.test.js
+++ test/units/game.test.js
@@ -2,9 +2,19 @@ const Game = require('../../scripts/game.js')
 
 const game = new Game();
 
-describe('#roll', () =>{
+describe('#roll', () => {
   test('It stores the result of a roll', ()=> {
     game.roll(10)
     expect(game.scorecard).toEqual([ 10 ])
   })
+})
+
+describe('#score', () => {
+  test('It returns the total score of the scorecard', () => {
+    game.roll(5)
+    game.roll(5)
+    game.roll(5)
+    expect(game.score()).toEqual(15)
+  })
+
 })
